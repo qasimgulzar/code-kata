4ad2846f55704124e0f8825194c2fbc6b05ea5ae
qasim <qasim.khokhar52@gmail.com>
2024-05-16 00:00:48 +0500

    added test case

--- cli_test.go
+++ cli_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"todo/cli/helpers/http"
+)
 
 func TestDetermineStartAndStepEven(t *testing.T) {
 	start, step := determineStartAndStep("Even")
@@ -13,5 +16,18 @@ func TestDetermineStartAndStepOdd(t *testing.T) {
 	start, step := determineStartAndStep("Odd")
 	if start != 1 || step != 2 {
 		t.Fatalf("Wrong value for step or start.")
+	}
+}
+
+func TestFetchTodo(t *testing.T) {
+	BaseURL := "https://jsonplaceholder.typicode.com"
+	client := http.TodoClient{BaseURL: BaseURL}
+	todo, err := client.Fetch(1)
+	if err != nil {
+		t.Fatalf("Error fetching todo.")
+	}
+
+	if todo.Id != 1 {
+		t.Fatalf("Error fetching correct todo.")
 	}
 }
